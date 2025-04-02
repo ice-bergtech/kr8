8bbe98b883e190166c05cc72b37c52fe09670917
b13rg <13488329+B13rg@users.noreply.github.com>
2025-04-01 22:37:24 -0700

    Add tests

--- bats-test/init_test.sh
+++ bats-test/init_test.sh
@@ -14,6 +14,26 @@ fi
   rm -rf ./init-test
 }
 
+@test "Check init cluster - named cluster" {
+  expected=$(<expected/init_cluster_temp-cluster)
+  mkdir -p ./init-test-cluster
+  run $KR8 init cluster -B ./init-test-cluster -o temp-cluster
+  [ "$status" -eq 0 ]
+  diff <(cat "init-test-cluster/clusters/temp-cluster/cluster.jsonnet") <(echo "$expected")
+  [ -d "init-test-cluster/clusters/" ]
+  rm -rf ./init-test-cluster
+}
+
+@test "Check init component - named component" {
+  expected=$(<expected/init_component_temp-component)
+  mkdir -p ./init-test-component
+  run $KR8 init component -B ./init-test-component -o temp-component
+  [ "$status" -eq 0 ]
+  [ -d "init-test-component/components/temp-component" ]
+  diff <(cat "init-test-component/components/temp-component/params.jsonnet") <(echo "$expected")
+  rm -rf ./init-test-component
+}
+
 # Remove this for now
 #  There's a weird race condition where it can check out "master" for kr8
 #  and cause problems.  That's going to be deeper in the code.
