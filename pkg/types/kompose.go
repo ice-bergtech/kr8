26b19801a55e7e675477dac258ca5547965b3a99
b13rg <b11484@gmail.com>
2025-04-03 17:12:03 -0700

    cleanpu type conversion

--- pkg/types/kompose.go
+++ pkg/types/kompose.go
@@ -10,69 +10,46 @@ import (
 // A struct describing a compose file that will be processed by kompose to produce kubernetes manifests
 // Based on https://github.com/kubernetes/kompose/blob/main/cmd/convert.go
 type KomposeConvertOptions struct {
-	// Kubernetes: Create a Helm chart for converted objects
-	CreateChart bool
 	// Kubernetes: Set the output controller ("deployment"|"daemonSet"|"replicationController")
 	Controller string
 
+	// The kubecfg (?) profile to use, can use multiple profiles
+	Profiles []string
+
+	// List of compose file filenames
+	InputFiles []string
+	// Specify a file name or directory to save objects to (if path does not exist, a file will be created)
+	OutFile string
+	// Generate a Helm chart for converted objects
+	CreateChart bool
+	// Add kompose annotations to generated resource
+	WithKomposeAnnotation bool
+	// Generate resource files into YAML format
+	GenerateYaml bool
+	// Spaces length to indent generated yaml files
+	YAMLIndent int
+	// Generate resource files into JSON format
+	GenerateJSON bool
 	// Print converted objects to stdout
 	ToStdout bool
 
 	// Set the type of build ("local"|"build-config"(OpenShift only)|"none")
 	Build string
-	// Openshift: Specify source repository for buildconfig (default remote origin)
-	BuildRepo string
-	// Openshift: Specify repository branch to use for buildconfig (default master)
-	BuildBranch string
 
-	// convertCmd.Flags().BoolVar(&ConvertDeploymentConfig, "deployment-config", true, "Generate an OpenShift deploymentconfig object")
-
-	Profiles  []string
-	PushImage bool
-	// Specify registry for pushing image, which will override registry from image name
-	PushImageRegistry string
-	// Generate resource files into YAML format
-	GenerateYaml bool
-	// Generate resource files into JSON format
-	GenerateJSON  bool
-	StoreManifest bool
+	// Specify the namespace of the generated resources`)
+	Namespace string
+	// Specify the number of replicas in the generated resource spec
+	Replicas int
+	// Convert docker-compose secrets into files instead of symlinked directories
+	SecretsAsFiles bool
 	// Use Empty Volumes. Do not generate PVCs
 	EmptyVols bool
 	// Volumes to be generated ("persistentVolumeClaim"|"emptyDir"|"hostPath" | "configMap")
 	Volumes string
 	// Specify the size of pvc storage requests in the generated resource spec
 	PVCRequestSize string
-	// Use an insecure Docker repository for OpenShift ImageStream
-	InsecureRepository bool
-	// Specify the number of replicas in the generated resource spec
-	Replicas int
-	// List of compose file filenames
-	InputFiles []string
-	// Specify a file name or directory to save objects to (if path does not exist, a file will be created)
-	OutFile  string
-	Provider string
-	// Specify the namespace of the generated resources`)
-	Namespace string
-
-	// convertCmd.Flags().BoolVar(&ConvertPushImage, "push-image", false, "If we should push the docker image we built")
-	// convertCmd.Flags().StringVar(&BuildCommand, "build-command", "", `Set the command used to build the container image, which will override the docker build command. Should be used in conjuction with --push-command flag.`)
-	// convertCmd.Flags().StringVar(&PushCommand, "push-command", "", `Set the command used to push the container image. override the docker push command. Should be used in conjuction with --build-command flag.`)
-
-	IsReplicationControllerFlag bool
-	IsReplicaSetFlag            bool
-	IsDeploymentConfigFlag      bool
-	IsNamespaceFlag             bool
-
-	BuildCommand string
-	PushCommand  string
-
-	Server string
-
-	// Spaces length to indent generated yaml files
-	YAMLIndent int
-
-	// Add kompose annotations to generated resource
-	WithKomposeAnnotation bool
+	// Specify whether to generate network policies or not
+	GenerateNetworkPolicies bool
 
 	// Create multiple containers grouped by 'kompose.service.group' label
 	MultipleContainerMode bool
@@ -81,17 +58,37 @@ type KomposeConvertOptions struct {
 	// Using with --service-group-mode=volume to specific a final service name for the group
 	ServiceGroupName string
 
-	// Convert docker-compose secrets into files instead of symlinked directories
-	SecretsAsFiles bool
-	// Specify whether to generate network policies or not
-	GenerateNetworkPolicies bool
+	// ??
+	Provider string
+	// ??
+	StoreManifest bool
+	// ??
+	Server string
+
+	// OpenShift: ??
+	CreateDeploymentConfig bool
+	// Openshift: Specify source repository for buildconfig (default remote origin)
+	BuildRepo string
+	// Openshift: Use an insecure Docker repository for OpenShift ImageStream
+	InsecureRepository bool
+	// Openshift: Specify repository branch to use for buildconfig (default master)
+	BuildBranch string
+
+	// Whether to push built docker image to remote registry.
+	PushImage bool
+	// Command used to build to image.  Used with PushCommand
+	BuildCommand string
+	// Command used to push image
+	PushCommand string
+	// Specify registry for pushing image, which will override registry from image name
+	PushImageRegistry string
 }
 
+// Initialie Kompose options with sensible defaults
 func Create(inputFiles []string, outDir string, cmp Kr8ComponentJsonnet) *KomposeConvertOptions {
 	return &KomposeConvertOptions{
 		CreateChart: false,
 		Controller:  "deployment",
-		CreateD:     true,
 		Replicas:    1,
 		Namespace:   cmp.Namespace,
 
@@ -113,11 +110,16 @@ func Create(inputFiles []string, outDir string, cmp Kr8ComponentJsonnet) *Kompos
 	}
 }
 
+// Generates a ConvertOptions struct that kompose expects from our commented KomposeConvertOptions
+// https://pkg.go.dev/github.com/kubernetes/kompose@v1.35.0/pkg/kobject#ConvertOptions
 func (k KomposeConvertOptions) genKomposePkgOpts() *kobject.ConvertOptions {
+	// https://pkg.go.dev/github.com/kubernetes/kompose@v1.35.0/pkg/kobject#ConvertOptions
 	var resultOpts kobject.ConvertOptions
 
+	resultOpts.ToStdout = k.ToStdout
+
 	resultOpts.InputFiles = k.InputFiles
-	resultOpts.Profiles = ""
+	resultOpts.Profiles = []string{}
 	resultOpts.Namespace = k.Namespace
 	resultOpts.Provider = k.Provider
 
@@ -125,17 +127,56 @@ func (k KomposeConvertOptions) genKomposePkgOpts() *kobject.ConvertOptions {
 	if k.Provider == "kubernetes" {
 		if k.Controller == "deployment" {
 			resultOpts.CreateD = true
+			resultOpts.IsDeploymentFlag = true
 		} else if k.Controller == "daemonSet" {
 			resultOpts.CreateDS = true
+			resultOpts.IsDaemonSetFlag = true
 		} else if k.Controller == "replicationController" {
 			resultOpts.CreateRC = true
+			resultOpts.IsReplicationControllerFlag = true
 		} else {
 			resultOpts.CreateD = true
 		}
 	} else if k.Provider == "openshift" {
+		resultOpts.CreateDeploymentConfig = k.CreateDeploymentConfig
+		resultOpts.BuildRepo = k.BuildRepo
+		resultOpts.BuildBranch = k.BuildBranch
 	}
 
-	//
+	resultOpts.PushImage = k.PushImage
+	resultOpts.PushImageRegistry = k.PushImageRegistry
+
+	resultOpts.CreateChart = k.CreateChart
+	resultOpts.GenerateYaml = k.GenerateYaml
+	resultOpts.GenerateJSON = k.GenerateJSON
+	resultOpts.StoreManifest = k.StoreManifest
+	resultOpts.EmptyVols = k.EmptyVols
+	resultOpts.Volumes = k.Volumes
+	resultOpts.PVCRequestSize = k.PVCRequestSize
+
+	resultOpts.InsecureRepository = k.InsecureRepository
+	resultOpts.Replicas = k.Replicas
+	resultOpts.InputFiles = k.InputFiles
+	resultOpts.OutFile = k.OutFile
+	resultOpts.Provider = k.Provider
+	resultOpts.Namespace = k.Namespace
+	resultOpts.Controller = k.Controller
+
+	resultOpts.BuildCommand = k.BuildCommand
+	resultOpts.PushCommand = k.PushCommand
+
+	resultOpts.Server = k.Server
+
+	resultOpts.YAMLIndent = k.YAMLIndent
+
+	resultOpts.WithKomposeAnnotation = k.WithKomposeAnnotation
+
+	resultOpts.MultipleContainerMode = k.MultipleContainerMode
+	resultOpts.ServiceGroupMode = k.ServiceGroupMode
+	resultOpts.ServiceGroupName = k.ServiceGroupName
+
+	resultOpts.SecretsAsFiles = k.SecretsAsFiles
+	resultOpts.GenerateNetworkPolicies = k.GenerateNetworkPolicies
 
 	return &resultOpts
 }
